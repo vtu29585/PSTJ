07906185820988f88046ae07e8e5e95638774a4c
vtu29585 <vtu29585@veltech.edu.in>
2026-08-22 09:40:38 +0530

    Add files via upload

--- solution.java
+++ solution.java
@@ -1,47 +1,19 @@
-import java.util.*;
+class Solution {
+    public boolean halvesAreAlike(String s) {
+        String vowels = "aeiouAEIOU";
+        int count = 0;
+        int n = s.length();
 
-class Player {
-	String name;
-	int score;
-
-	Player(String name, int score) {
-		this.name = name;
-		this.score = score;
-	}
-}
-
-class Checker implements Comparator<Player> {
-
-    @Override
-    public int compare(Player a, Player b) {
-
-        // Higher score comes first
-        if (a.score != b.score) {
-            return b.score - a.score;
+        for (int i = 0; i < n / 2; i++) {
+            if (vowels.indexOf(s.charAt(i)) != -1)
+                count++;
         }
 
-        // Same score: alphabetical order
-        return a.name.compareTo(b.name);
-    }
-}
-
-public class Solution {
-
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt();
-
-        Player[] player = new Player[n];
-        Checker checker = new Checker();
-        
-        for(int i = 0; i < n; i++){
-            player[i] = new Player(scan.next(), scan.nextInt());
+        for (int i = n / 2; i < n; i++) {
+            if (vowels.indexOf(s.charAt(i)) != -1)
+                count--;
         }
-        scan.close();
 
-        Arrays.sort(player, checker);
-        for(int i = 0; i < player.length; i++){
-            System.out.printf("%s %s\n", player[i].name, player[i].score);
-        }
+        return count == 0;
     }
 }
