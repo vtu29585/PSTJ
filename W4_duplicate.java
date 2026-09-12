a448c9a67b4f03035e545da71044fefa88e86212
vtu29585 <vtu29585@veltech.edu.in>
2026-09-12 09:19:54 +0530

    Update W4_duplicate.java

--- W4_duplicate.java
+++ W4_duplicate.java
@@ -7,16 +7,14 @@ class Solution {
 
         for (int num : nums) {
 
-            // If number is already present
             if (set.contains(num)) {
                 return true;
             }
 
-            // Add number to set
+           
             set.add(num);
         }
 
-        // No duplicate found
         return false;
     }
 }
