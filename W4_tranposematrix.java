b6f2bcaaa44f8eee84d2e91a9f3ee724d7cedf79
vtu29585 <vtu29585@veltech.edu.in>
2026-09-12 09:19:08 +0530

    Update W4_tranposematrix.java

--- W4_tranposematrix.java
+++ W4_tranposematrix.java
@@ -1,8 +1,8 @@
 class Solution {
     public int[][] transpose(int[][] matrix) {
 
-        int m = matrix.length;       // number of rows
-        int n = matrix[0].length;    // number of columns
+        int m = matrix.length;     
+        int n = matrix[0].length;   
 
         int[][] result = new int[n][m];
 
