c2a1097924dc34d58385826208f66de4a588d7fc
vtu29585 <vtu29585@veltech.edu.in>
2026-09-12 09:42:55 +0530

    Add files via upload

---
+++ W5_circularsubaray.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int maxSubarraySumCircular(int[] nums) {
+
+        int totalSum = 0;
+
+        int currentMax = 0;
+        int maxSum = nums[0];
+
+        int currentMin = 0;
+        int minSum = nums[0];
+
+        for (int num : nums) {
+
+            // Maximum subarray sum
+            currentMax = Math.max(num, currentMax + num);
+            maxSum = Math.max(maxSum, currentMax);
+
+            // Minimum subarray sum
+            currentMin = Math.min(num, currentMin + num);
+            minSum = Math.min(minSum, currentMin);
+
+            totalSum += num;
+        }
+
+        // All elements are negative
+        if (maxSum < 0) {
+            return maxSum;
+        }
+
+        // Maximum circular subarray sum
+        int circularSum = totalSum - minSum;
+
+        return Math.max(maxSum, circularSum);
+    }
+} 
+
