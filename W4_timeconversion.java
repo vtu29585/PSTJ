79af95dfb488b451dbb2a2e654e20d8b3d838cdb
vtu29585 <vtu29585@veltech.edu.in>
2026-09-12 08:53:09 +0530

    Add files via upload

---
+++ W4_timeconversion.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Main {
+
+    public static String timeConversion(String s) {
+        String period = s.substring(8, 10); // AM or PM
+        int hour = Integer.parseInt(s.substring(0, 2));
+
+        if (period.equals("AM")) {
+            if (hour == 12) {
+                hour = 0;
+            }
+        } else { // PM
+            if (hour != 12) {
+                hour += 12;
+            }
+        }
+
+        return String.format("%02d", hour) + s.substring(2, 8);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.nextLine();
+
+        System.out.println(timeConversion(s));
+
+        sc.close();
+    }
+}
